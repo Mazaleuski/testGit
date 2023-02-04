59b549b5fc76fe35d86bffe10cc5d9967c20ad02
Mazaleuski <mazaleuski.ya@gmail.com>
2023-02-04 18:42:53 +0300

    Initial commit

---
+++ src/by/teachmeskills/homeworks/hw_10022023/HomeworkClass.java
@@ -0,0 +1,10 @@
+package by.teachmeskills.homeworks.hw_10022023;
+
+public class HomeworkClass {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
