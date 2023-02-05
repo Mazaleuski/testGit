61e7c3a733ab057729be1ccd35ccaa731ddcf9e4
Mazaleuski <mazaleuski.ya@gmail.com>
2023-02-05 12:33:39 +0300

    Subtraction

--- src/by/teachmeskills/homeworks/hw_10022023/HomeworkClass.java
+++ src/by/teachmeskills/homeworks/hw_10022023/HomeworkClass.java
@@ -4,7 +4,7 @@ public class HomeworkClass {
     public static void main(String[] args) {
         int a = 7;
         int b = 9;
-        int sum = a + b;
-        System.out.println("Sum: " + sum);
+        int subtract = a - b;
+        System.out.println("Difference: " + subtract);
     }
 }
